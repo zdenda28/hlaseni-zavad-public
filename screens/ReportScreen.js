477eca74519f4f55e2d555bf5243c8cebfb530e1
Zdeněk Tomka <tomz03@vse.cz>
2019-05-25 12:40:06 +0200

    issue fixed: allowed chars '#' and '%'

--- screens/ReportScreen.js
+++ screens/ReportScreen.js
@@ -278,7 +278,7 @@ export default class ReportScreen extends React.Component {
 
     validate = () => {
         let valid = true;
-        const SPECIAL_CHARS = /[@#$%^&*<>/{}|]/g;
+        const SPECIAL_CHARS = /[@$^&*<>/{}|]/g;
         const EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
 
         //validace názvu závady
